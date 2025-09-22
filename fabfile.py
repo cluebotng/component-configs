3a32ab94d6ca9e2b40bb241464e4f44e8837a26e
Damian Zaremba <damian@damianzaremba.co.uk>
2025-09-22 16:17:59 +0200

    ci - update trigger paths

--- fabfile.py
+++ fabfile.py
@@ -319,7 +319,12 @@ def _execute_deployment(tool_name: str, deploy_token: str) -> bool:
 def _generate_workflow(tool_name: str):
     # We do this to avoid `yaml` as a dep, it's simple enough
     config = f"name: 'Trigger deploy for {tool_name}'\n"
-    config += f"on: {{ push: {{ branches: [ main ], paths: [ '{tool_name}.yaml', '.github/workflows/{tool_name}.yaml' ] }} }}\n"
+    config += f"on: {{ push: {{ branches: [ main ], paths: [ "
+    config += f"'{tool_name}.yaml', "
+    config += f"'config/network-policies/{tool_name}.yaml', "
+    config += f"'config/web-services/{tool_name}.yaml', "
+    config += f"'.github/workflows/{tool_name}.yaml' "
+    config += f"] }} }}\n"
     config += "jobs:\n"
     # Until T401868 is resolved, update the tool with the config we want deployed
     # Note: the config will not be re-fetched as `source_url` cannot be rewritten on the same sha... so this is '1 off'
