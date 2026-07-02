d8f948e5294e9a2a4bb1edbd6a5ffaa6c92fe3a7
Damian Zaremba <damian@damianzaremba.co.uk>
2026-07-02 22:32:08 +0200

    copy-worker-credentials-to-env - dynamically lookup users

--- config/static-files/files/copy-worker-credentials-to-env.sh
+++ config/static-files/files/copy-worker-credentials-to-env.sh
@@ -5,17 +5,38 @@
 set -eu
 set -o pipefail
 
-credentials=""
-for i in {1..10}
-do
-    username="tools.cluebotng-worker-$i"
-    echo "Fetching credentials for ${username}"
-    user=$(/usr/bin/sudo -u "${username}" -- toolforge envvars show --raw TOOL_REPLICA_USER)
-    password=$(/usr/bin/sudo -u "${username}" -- toolforge envvars show --raw TOOL_REPLICA_PASSWORD)
+worker_accounts=$(
+    ldapsearch \
+        -x -LL \
+        -b 'ou=people,ou=servicegroups,dc=wikimedia,dc=org' \
+        'uid=tools.cluebotng-worker-*' 'uid' | \
+        awk '/^uid: /{sub(/^uid: /, ""); print}'
+)
 
-    [[ -z "${credentials}" ]] || credentials="${credentials}, "
-    credentials="${credentials}{\"user\": \"${user}\", \"pass\": \"${password}\"}"
+all_credentials=""
+for username in ${worker_accounts}
+do
+    echo "Fetching credentials for ${username}"
+    user_envvars=$(
+        /usr/bin/sudo -u "${username}" -- \
+            toolforge envvars list --json | \
+            jq '[.envvars[] | select(.name | IN("TOOL_REPLICA_USER","TOOL_REPLICA_PASSWORD"))]
+                | map({(.name): .value})
+                | add'
+    )
+
+    database_username=$(jq -r '.TOOL_REPLICA_USER' <<< "$user_envvars")
+    database_password=$(jq -r '.TOOL_REPLICA_PASSWORD' <<< "$user_envvars")
+    if [[ -n "$database_username" ]] && [[ -n "$database_password" ]];
+    then
+        [[ -z "${all_credentials}" ]] || all_credentials="${all_credentials}, "
+        all_credentials="${all_credentials}{\"user\": \"${database_username}\", \"pass\": \"${database_password}\"}"
+    fi
 done
 
-echo "Updating CBNG_BOT_MYSQL_CREDENTIALS"
-toolforge envvars create CBNG_BOT_MYSQL_CREDENTIALS <<< "[${credentials}]" > /dev/null
+if [[ -n "$all_credentials" ]];
+then
+    echo "Updating CBNG_BOT_MYSQL_CREDENTIALS"
+    jq -c '. | sort_by(.name)' <<< "[${all_credentials}]" | \
+    toolforge envvars create CBNG_BOT_MYSQL_CREDENTIALS > /dev/null
+fi
